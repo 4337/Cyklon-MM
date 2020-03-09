76b6d5386b69517b47f8ec93f3222504671c1656
4337 <tao.ceee.tung@gmail.com>
2020-03-09 21:36:53 +0100

    Update IO.py

--- IO.py
+++ IO.py
@@ -86,7 +86,7 @@ class IO :
       def __lock__() :
 
           try :
-               hnd = open( os.path.dirnameh(__file__) + '\\.lock','w' )
+               hnd = open( os.path.dirnameh(sys.argv[0]) + '\\.lock','w' )
                hnd.write('lockme')
                hnd.close()
           except :
