f0ef087d681d6e32772496cba2cde11e06fb2928
4337 <tao.ceee.tung@gmail.com>
2020-03-14 12:07:22 +0100

    Update

--- RUN.cmd
+++ RUN.cmd
@@ -2,10 +2,12 @@ if exist .lock (
    goto __skip__
 )
 
-powershell -executionpolicy bypass -File E:\Monitor\at_kill.ps1  
+powershell -executionpolicy bypass -File C:\Users\48663\Desktop\Origin\Domato\at_kill.ps1  
 del /Q /S %temp%\* 
 rmdir /Q /S %temp%\*
-start "C:\Python27\python.exe" "E:\Monitor\Main.py" -browser "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe"
+start "C:\Python27\python.exe" "C:\Users\48663\Desktop\Origin\Domato\Main.py" -browser "C:\Program Files\Mozilla Firefox\firefox.exe" -port 80 -symbols http://msdl.microsoft.com/download/symbols;https://symbols.mozilla.org/
+
+REM  "C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\MicrosoftEdge.exe"
 
 REM "C:\Program Files\Mozilla Firefox\firefox.exe" -port 80 -symbols http://msdl.microsoft.com/download/symbols;https://symbols.mozilla.org/
 
