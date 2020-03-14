d2eba56e1abf228ea75396b6bf482984b4f67c7c
4337 <tao.ceee.tung@gmail.com>
2020-03-14 12:54:20 +0100

    Update JITDbg.py

--- JITDbg.py
+++ JITDbg.py
@@ -3,7 +3,7 @@
 '''
 Pierwszy na świecie debugger filtrujący hehehe lol
 Ogólnie fajna koncepcja, zamiast tworzyć skomplikowane oprogramowanie monitorujące
-Piszemy fragment kodu debugger instalujemy go jako "Just In Time Debugger" i filtrujemy 
+Piszemy fragment kodu debuggera instalujemy go jako "Just In Time Debugger" i filtrujemy 
 procesy. Jeżeli crash występuje w jednym z procesów które nas interesują to robimy "coś"
 np. logujemy crash_dump do pliku kopiujemy 'repro' etc, jeżeli nie to odłączamy debugger i olewamy 
 process. Dodatkowy plus to fakt, że nie musimy się w ogóle przejmować procesami potomnymi ^^
